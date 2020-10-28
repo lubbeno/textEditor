60f2aa52cb571b29f4370f6a584009c799891107
lubossukup <lubos.sukup@cz.ibm.com>
2020-10-28 12:39:51 +0100

    pridane riesenie

--- src/textEditor/TextEditorController.java
+++ src/textEditor/TextEditorController.java
@@ -1,36 +1,89 @@
 package textEditor;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.skin.TextFieldSkin;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextEditorController {
 
     @FXML
-    TextArea myTextFile;
+    ListView fileList;
 
+    @FXML
+    TextArea myTextFile;
 
     @FXML
     public void save() {
- /*       dostat text z textArea
-        dopisat metodu na zapis do suboru.
-        Pouzit Bufferedwriter na zapis do dokumentu */
-        //nejaka zmena
+        String text =myTextFile.getText();
 
-        String text = myTextFile.getText();
-        writer1.write(text);
+        File selectedFile = (File) fileList.getSelectionModel().getSelectedItem();
+
+        File subor = new File("a.txt");
+
+        try (Writer writer = new FileWriter(selectedFile)) {
+            writer.write(text);
+
+        }catch (IOException ex){
+
+        }
+    }
+
+    public void initialize() {
+        //korenovy adresar CalcualtorAdv
+        File file = new File(".");
+
+        File[] files = file.listFiles();
+
+        List<File> poleSuborov = new ArrayList<>();
+
+        for (File elementPolafiles : files) {
+            if (elementPolafiles.isFile()) {
+                poleSuborov.add(elementPolafiles);
+            }
+        }
+
+
+        ObservableList<File> list2 = FXCollections.observableList(poleSuborov);
+
+        fileList.setItems(list2);
+
+        SelectionModel<File> selectionModel = fileList.getSelectionModel();
+        selectionModel.selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+            try {
+                viewFile(newValue);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 
-    public void initialize(){
-  /*      Nacitat textovy dokument
-         zobrazit v textarea*/
+    public void viewFile(File file) throws FileNotFoundException {
 
+        String strBuffer;
 
+        String out = "";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            while ((strBuffer = bufferedReader.readLine()) != null) {
+                System.out.println(strBuffer);
+
+                out = out + strBuffer + "\n";
+            }
+            myTextFile.setText(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
-    void test(){
-
-    }
-
 }
